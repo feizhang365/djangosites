fd881cc23d0d22e3ad15c62cc110ebcb8707c000
fzh <>
2018-03-07 15:14:44 +0800

    finish poll app

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,25 @@
+"""
+Admin
+"""
 from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Customize Question Admin form
+    """
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,48 +2,71 @@
 views
 """
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views import generic
+from django.utils import timezone
 # Create your views here.
 # from django.template import loader
 from .models import Choice, Question
 
 
 # def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-
-# def index(request):
+#     """
+#     index page.
+#     """
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     output = ', '.join([q.question_text for q in latest_question_list])
-#     return HttpResponse(output)
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html', context)
 
-def index(request):
-    """
-    index page.
-    """
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+# def detail(request, question_id):
+#     """
+#      detail page view
+#     """
+#     # return HttpResponse("You're looking at question %s. detail" % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
-def detail(request, question_id):
-    """
-     detail page view
-    """
-    # return HttpResponse("You're looking at question %s. detail" % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def results(request, question_id):
+#     """
+#     result page view
+#     """
+#     response = "You're looking at the results of question %s. result"
+#     return HttpResponse(response % question_id)
 
-def results(request, question_id):
+### define index and view
+class IndexView(generic.ListView):
     """
-    result page view
+    index view
     """
-    response = "You're looking at the results of question %s. result"
-    return HttpResponse(response % question_id)
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+
+class DetailView(generic.DetailView):
+    """
+    detail view
+    """
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    """
+    result view
+    """
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     """
