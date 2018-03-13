8f982bfa69178bf2cedf1ef6db97a3e2ed2d74fe
fzh <>
2018-03-13 15:44:46 +0800

    add logic

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -8,18 +8,21 @@ from django.contrib import admin
 from .models import Choice, Question
 
 class ChoiceInline(admin.TabularInline):
+    """
+     choice
+    """
     model = Choice
     extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     """
     Customize Question Admin form
     """
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-
